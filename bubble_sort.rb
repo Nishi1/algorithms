b150a93503476a61ecfce5d4f9e8472ff70c4c11
Nishi Kant Sharma <nishiit24@gmail.com>
2019-04-03 15:59:15 +0530

    Adding comments to file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,10 @@
+=begin
+Bubble sort is a simple sorting algorithm. This sorting algorithm is comparison-based algorithm in
+which each pair of adjacent elements is compared and the elements are swapped if they are not in 
+order. This algorithm is not suitable for large data sets as its average and worst case complexity
+are of Ο(n2) where n is the number of items.
+=end
+
 arr = [15,3,10,2,4,0,8]
 
 arr_size = arr.size
